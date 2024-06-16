6e4a8e15364c9ef409347b9b2106e31877ed37d2
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-16 22:46:45 +0200

    Update and add home-manager

--- flake.nix
+++ flake.nix
@@ -2,18 +2,29 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # NixOS official package source
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.T460p = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      T460p = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+
+	  home-manager.nixosModules.home-manager {
+             home-manager.users.ma-gerbig = import ./home/ma-gerbig;
+	  }
+        ];
+      };
     };
   };
 }
