35c7c050edc8efd98a25828dd74a437ecd70cab0
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-16 20:58:06 +0200

    Enable Nix Flake Feature

--- configuration.nix
+++ configuration.nix
@@ -26,17 +26,22 @@
     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   };
 
-  networking.hostId = "d5a98f2e";
-  networking.hostName = "T460p"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  networking = {
+    # Define your hostname.
+    hostName = "T460p";
+    # The 32-bit host ID of the machine
+    # needed for zfs
+    hostId = "d5a98f2e";
+    # Enable networking
+    networkmanager.enable = true;
+    # Enables wireless support via wpa_supplicant.
+    # wireless.enable = true;
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # Nix Settings
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -63,15 +68,14 @@
   services = {
     xserver = {
       enable = true;
-      
       # Configure keymap in X11
-      layout = "de";
-      xkbVariant = "";
-      
+      xkb = {
+        layout = "de";
+        variant = "";
+      };
       displayManager = {
 	gdm.enable = true;
       };
-     
       desktopManager = {
         gnome.enable = true; 
       };
@@ -95,6 +99,7 @@
     neovim
     firefox
     git
+    curl
     wget
     btop
   ];
