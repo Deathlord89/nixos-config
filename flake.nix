f4406942ada2c1a70dbb6f55017520ca1079b854
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-19 18:45:06 +0200

    Add nixos-hardware and disko as input

--- flake.nix
+++ flake.nix
@@ -4,9 +4,16 @@
   inputs = {
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    # Declarative disk partitioning
+    disko = {
+      url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -23,13 +30,18 @@
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
+  outputs = inputs @ { self, nixpkgs, nixos-hardware, disko, home-manager, ... }: {
     nixosConfigurations = {
       T460p = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/T460p
+
+          # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+          nixos-hardware.nixosModules.lenovo-thinkpad-t460p
+
+          disko.nixosModules.disko
 
           home-manager.nixosModules.home-manager {
             home-manager.users.ma-gerbig = import ./home/ma-gerbig;
