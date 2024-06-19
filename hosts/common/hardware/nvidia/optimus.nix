a9543fe2c1fca0ae8a28f3604e68683067d570e8
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-19 17:47:59 +0200

    Add nvidia driver and optimus config

---
+++ hosts/common/hardware/nvidia/optimus.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }:
+
+{
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    # Make sure to use the correct Bus ID values for your system!
+    # Use: nix shell nixpkgs#lshw -c sudo lshw -c display
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+  };
+}
