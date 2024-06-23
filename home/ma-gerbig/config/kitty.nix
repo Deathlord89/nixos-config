6f9e3bc80b454e20bad7482ad8c561fb3a2ae057
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-23 23:26:55 +0200

    Install lazygit

--- home/ma-gerbig/config/kitty.nix
+++ home/ma-gerbig/config/kitty.nix
@@ -13,7 +13,7 @@ in {
       background_opacity = "0.75";
       linux_display_server = "x11";
       term = "xterm-256color";
-      font_size = "9.0";
+      font_size = "11.0";
       font_family = "${font}";
       bold_font = "${font} Bold";
       italic_font = "${font} Italic";
