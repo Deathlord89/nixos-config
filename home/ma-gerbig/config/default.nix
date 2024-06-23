82d8dd4ef19d6fd87db1cdbfecedec2fad45f37c
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-06-23 18:56:22 +0200

    Update kitty config and add nerdfonts

--- home/ma-gerbig/config/default.nix
+++ home/ma-gerbig/config/default.nix
@@ -1,9 +1,13 @@
-{
+{pkgs, ...}: {
   imports = [
     ./bash.nix
     ./dconf.nix
     ./git.nix
     ./gpg.nix
     ./kitty.nix
+  ];
+  fonts.fontconfig.enable = true;
+  home.packages = [
+    (pkgs.nerdfonts.override {fonts = ["FiraCode"];})
   ];
 }
--- home/ma-gerbig/config/kitty.nix
+++ home/ma-gerbig/config/kitty.nix
@@ -4,13 +4,11 @@
   lib,
   ...
 }: {
-  home.packages = [pkgs.kitty];
-
   programs.kitty = {
     enable = true;
     settings = {
       allow_remote_control = "no";
-      background_opacity = "0.8";
+      background_opacity = "0.75";
       linux_display_server = "x11";
       term = "xterm-256color";
     };
