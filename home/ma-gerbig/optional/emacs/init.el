428c0f143f9fcfdfde639fc459711174fd7a5090
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-07-13 17:01:04 +0200

    emacs: add usefull org-mode additions

--- home/ma-gerbig/optional/emacs/init.el
+++ home/ma-gerbig/optional/emacs/init.el
@@ -447,12 +447,137 @@
                (mapcar #'(lambda (file) (file-name-concat org-directory file))
                        '("Agenda.org"
                          "Inbox.org"))))
+  (org-todo-keywords
+   '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+     (sequence "|" "WAIT(w)" "BACK(b)")))
+  (org-agenda-start-with-log-mode t)
+  (org-log-done 'time)
+  (org-log-into-drawer t)
+  (org-startup-indented t)
+  (org-src-fontify-natively t)
+  (org-src-tab-acts-natively t)
+  (org-hide-emphasis-markers t)
+  (prettify-symbols-unprettify-at-point 'right-edge)
   ;;:custom-face
   
-  ;;:hook
+  :hook
+  (org-babel-after-execute . org-redisplay-inline-images)
+  (org-mode . (lambda ()
+                "Beautify Org Checkbox Symbol"
+                (push '("[ ]" . "☐" ) prettify-symbols-alist)
+                (push '("[X]" . "☑" ) prettify-symbols-alist)
+                (push '("[-]" . "⊡" ) prettify-symbols-alist)
+                (prettify-symbols-mode)))
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (shell . t)
+     (latex . t)
+     (org . t)))
+  (font-lock-add-keywords
+   'org-mode
+   '(("^ *\\([-]\\) "
+      (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  (defface org-checkbox-done-text
+    '((t (:foreground "#71696A" :strike-through t)))
+    "Face for the text part of a checked org-mode checkbox.")
   
-  ;;:config
-  )
+  (font-lock-add-keywords
+   'org-mode
+   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+      1 'org-checkbox-done-text prepend))
+   'append)
+  (setq org-ellipsis " ▾"))
+
+(use-package org-appear
+  :hook (org-mode . org-appear-mode))
+
+(with-eval-after-load 'org
+  (defvar-local rasmus/org-at-src-begin -1
+    "Variable that holds whether last position was a ")
+
+  (defvar rasmus/ob-header-symbol ?☰
+    "Symbol used for babel headers")
+
+  (defun rasmus/org-prettify-src--update ()
+    (let ((case-fold-search t)
+          (re "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*")
+          found)
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward re nil t)
+          (goto-char (match-end 0))
+          (let ((args (org-trim
+                       (buffer-substring-no-properties (point)
+                                                       (line-end-position)))))
+            (when (org-string-nw-p args)
+              (let ((new-cell (cons args rasmus/ob-header-symbol)))
+                (cl-pushnew new-cell prettify-symbols-alist :test #'equal)
+                (cl-pushnew new-cell found :test #'equal)))))
+        (setq prettify-symbols-alist
+              (cl-set-difference prettify-symbols-alist
+                                 (cl-set-difference
+                                  (cl-remove-if-not
+                                   (lambda (elm)
+                                     (eq (cdr elm) rasmus/ob-header-symbol))
+                                   prettify-symbols-alist)
+                                  found :test #'equal)))
+        ;; Clean up old font-lock-keywords.
+        (font-lock-remove-keywords nil prettify-symbols--keywords)
+        (setq prettify-symbols--keywords (prettify-symbols--make-keywords))
+        (font-lock-add-keywords nil prettify-symbols--keywords)
+        (while (re-search-forward re nil t)
+          (font-lock-flush (line-beginning-position) (line-end-position))))))
+
+  (defun rasmus/org-prettify-src ()
+    "Hide src options via `prettify-symbols-mode'.
+
+        `prettify-symbols-mode' is used because it has uncollpasing. It's
+        may not be efficient."
+    (let* ((case-fold-search t)
+           (at-src-block (save-excursion
+                           (beginning-of-line)
+                           (looking-at "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*"))))
+      ;; Test if we moved out of a block.
+      (when (or (and rasmus/org-at-src-begin
+                     (not at-src-block))
+                ;; File was just opened.
+                (eq rasmus/org-at-src-begin -1))
+        (rasmus/org-prettify-src--update))
+      ;; Remove composition if at line; doesn't work properly.
+      ;; (when at-src-block
+      ;;   (with-silent-modifications
+      ;;     (remove-text-properties (match-end 0)
+      ;;                             (1+ (line-end-position))
+      ;;                             '(composition))))
+      (setq rasmus/org-at-src-begin at-src-block)))
+
+  ;; This function helps to produce a single glyph out of a
+  ;; string. The glyph can then be used in prettify-symbols-alist.
+  ;; This function was provided by Ihor in the org-mode mailing list.
+  (defun yant/str-to-glyph (str)
+    "Transform string into glyph, displayed correctly."
+    (let ((composition nil))
+      (dolist (char (string-to-list str)
+                    (nreverse (cdr composition)))
+        (push char composition)
+        (push '(Br . Bl) composition))))
+
+  (defun rasmus/org-prettify-symbols ()
+    (mapc (apply-partially 'add-to-list 'prettify-symbols-alist)
+          (cl-reduce 'append
+                     (mapcar (lambda (x) (list x (cons (upcase (car x)) (cdr x))))
+                             `(("#+begin_src" . ?⎡) ;; ⎡ ➤ 🖝 ➟ ➤ ✎
+                               ;; multi-character strings can be used with something like this:
+                               ;; ("#+begin_src" . ,(yant/str-to-glyph "```"))
+                               ("#+end_src"   . ?⎣) ;; ⎣ ✐
+                               ("#+header:" . ,rasmus/ob-header-symbol)
+                               ("#+begin_quote" . ?«)
+                               ("#+end_quote" . ?»)))))
+    (turn-on-prettify-symbols-mode)
+    (add-hook 'post-command-hook 'rasmus/org-prettify-src t t))
+  (add-hook 'org-mode-hook #'rasmus/org-prettify-symbols))
 
 (use-package projectile
   :diminish projectile-mode
