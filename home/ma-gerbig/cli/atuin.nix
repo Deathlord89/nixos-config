e651248589d599b398f15911fd180c8498c64322
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-10-24 15:09:44 +0200

    Atuin: Config changes

--- home/ma-gerbig/cli/atuin.nix
+++ home/ma-gerbig/cli/atuin.nix
@@ -3,5 +3,10 @@
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
+    settings = {
+      enter_accept = true;
+      filter_mode_shell_up_key_binding = "host";
+      records = true;
+    };
   };
 }
