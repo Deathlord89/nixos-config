187b6388cda34c5d02182c9b37a862e64f536c51
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-10-26 12:48:36 +0200

    Block Hibernate and hybrid-sleep with zram

--- hosts/common/base/system.nix
+++ hosts/common/base/system.nix
@@ -40,6 +40,11 @@
     #media-session.enable = true;
   };
 
+  # Hibernate and hybrid-sleep won't work correctly without
+  # an on-disk swap.
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # Enable in-memory compressed swap device
   zramSwap.enable = true;
 
