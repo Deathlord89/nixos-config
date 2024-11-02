a787690e8eab848d9e88c4f4ae7c5590ebd86414
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-11-02 21:36:38 +0100

    Syncthing enable default folder

--- hosts/Maximus/services/syncthing.nix
+++ hosts/Maximus/services/syncthing.nix
@@ -9,14 +9,17 @@
         devices = {
           # Existing devices here!
           steamdeck.id = "IGM62YB-NUY5QXD-QRBBMQX-PFWSSZG-5NEN3ZQ-XH6CGWW-3DWS2TA-DMW77AG";
-          t460p.id = "7H7UMNA-IBPSUGV-7ZSSPAR-YSFK6H4-SMKVZD6-6N5ABIV-FYTQSFU-K5QRZAQ";
+          T460p.id = "7H7UMNA-IBPSUGV-7ZSSPAR-YSFK6H4-SMKVZD6-6N5ABIV-FYTQSFU-K5QRZAQ";
         };
 
         folders = {
           "Default Folder" = {
             id = "default";
             path = "/home/ma-gerbig/Sync";
-            devices = ["steamdeck"];
+            devices = [
+              "steamdeck"
+              "T460p"
+            ];
           };
         };
       };
--- hosts/T460p/services/syncthing.nix
+++ hosts/T460p/services/syncthing.nix
@@ -8,16 +8,20 @@
       settings = {
         devices = {
           # Existing devices here!
+          Maximus.id = "FM3Q33R-NTLIUEM-LWEIQJV-GAJHS4X-CXMPCF4-KVGSCIK-BDLDOQJ-5HUX5QD";
           steamdeck.id = "IGM62YB-NUY5QXD-QRBBMQX-PFWSSZG-5NEN3ZQ-XH6CGWW-3DWS2TA-DMW77AG";
         };
 
-        #folders = {
-        #  "Default Folder" = {
-        #    id = "default";
-        #    path = "/home/ma-gerbig/Sync";
-        #    devices = ["steamdeck"];
-        #  };
-        #};
+        folders = {
+          "Default Folder" = {
+            id = "default";
+            path = "/home/ma-gerbig/Sync";
+            devices = [
+              "Maximus"
+              "steamdeck"
+            ];
+          };
+        };
       };
     };
   };
