9391c3ce2adfeba5633eaa582e040b97c5ca16f9
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-11-03 10:48:43 +0100

    Enable sops-nix user's password

--- hosts/common/users/ma-gerbig/default.nix
+++ hosts/common/users/ma-gerbig/default.nix
@@ -7,11 +7,11 @@
 }: let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
-    #mutableUsers = false;
+    mutableUsers = false;
     users.ma-gerbig = {
       isNormalUser = true;
+      shell = pkgs.fish;
       description = "Marc-André Gerbig";
       extraGroups =
         [
@@ -26,14 +26,13 @@ in {
           "video"
         ];
 
-      #hashedPasswordFile = config.sops.secrets."password/ma-gerbig".path;
+      hashedPasswordFile = config.sops.secrets."password/ma-gerbig".path;
       openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ../../../../home/ma-gerbig/ssh.pub);
       packages = [pkgs.home-manager];
-      shell = pkgs.fish;
     };
   };
 
-  #sops.secrets."password/ma-gerbig".neededForUsers = true;
+  sops.secrets."password/ma-gerbig".neededForUsers = true;
 
   home-manager = {
     users.ma-gerbig = import ../../../../home/ma-gerbig/${config.networking.hostName}.nix;
