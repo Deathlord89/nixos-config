7426f846924436d40f46dae71ad8505a8dd8907f
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-12-20 21:37:47 +0100

    Fix atuin zfs performance

--- home/ma-gerbig/cli/atuin.nix
+++ home/ma-gerbig/cli/atuin.nix
@@ -1,4 +1,8 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.atuin = {
     enable = true;
     enableBashIntegration = true;
@@ -10,6 +14,36 @@
       sync_frequency = "30m";
       secrets_filter = true;
       records = true;
+      daemon = {
+        enabled = true;
+        systemd_socket = true;
+      };
+    };
+  };
+
+  # Fix for bad zfs performance
+  # https://forum.atuin.sh/t/getting-the-daemon-working-on-nixos/334/5
+  systemd.user = let
+    atuinSockDir = "${config.home.homeDirectory}/.local/share/atuin";
+    atuinSock = "${atuinSockDir}/atuin.sock";
+    unitConfig = {
+      Description = "Atuin Magical Shell History Daemon";
+      ConditionPathIsDirectory = atuinSockDir;
+      ConditionPathExists = "${config.home.homeDirectory}/.config/atuin/config.toml";
+    };
+  in {
+    sockets.atuin-daemon = {
+      Unit = unitConfig;
+      Install.WantedBy = ["default.target"];
+      Socket = {
+        ListenStream = atuinSock;
+        Accept = false;
+        SocketMode = "0600";
+      };
+    };
+    services.atuin-daemon = {
+      Unit = unitConfig;
+      Service.ExecStart = "${pkgs.atuin}/bin/atuin daemon";
     };
   };
 }
