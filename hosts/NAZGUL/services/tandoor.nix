66dc9ef2ee60760bb90c8e63c55de74b112bab3c
Marc-André Gerbig <marc.gerbig@gmail.com>
2024-12-25 17:38:07 +0100

    Fix nginx tandoor issue

--- hosts/NAZGUL/services/tandoor.nix
+++ hosts/NAZGUL/services/tandoor.nix
@@ -1,11 +1,21 @@
 {
+  config,
+  lib,
+  ...
+}: {
+  sops.secrets = {
+    "tandoor/web_env.enc" = {
+      sopsFile = ../secrets.yaml;
+    };
+  };
+
   services = {
     tandoor-recipes = {
       enable = true;
-      address = "0.0.0.0";
-      port = 8082;
+      port = 3030;
       extraConfig = {
-        # ALLOWED_HOSTS=recipes.mydomain.com
+        CSRF_TRUSTED_ORIGINS = "https://recipes.ma-gerbig.de";
+        #ALLOWED_HOSTS = "recipes.ma-gerbig.de";
         DB_ENGINE = "django.db.backends.postgresql";
         POSTGRES_DB = "tandoor_recipes";
         POSTGRES_USER = "tandoor_recipes";
@@ -22,6 +32,46 @@
       ];
       ensureDatabases = ["tandoor_recipes"];
     };
+
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "recipes.ma-gerbig.de" = {
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 8082;
+            }
+          ];
+          locations = {
+            "/media/".alias = "/var/lib/tandoor-recipes/";
+            "/" = {
+              proxyPass = "http://127.0.0.1:3030";
+              extraConfig = ''
+                proxy_set_header Host $host;
+                proxy_set_header X-Forwarded-Proto https;
+              '';
+            };
+          };
+        };
+      };
+    };
+  };
+
+  # Fix for nginx folder permissions
+  users = {
+    users.tandoor_recipes = {
+      name = "tandoor_recipes";
+      group = "tandoor_recipes";
+      isSystemUser = true;
+    };
+    groups.tandoor_recipes.members = ["${config.services.nginx.user}"];
+  };
+
+  systemd.services.tandoor-recipes.serviceConfig = {
+    EnvironmentFile = "${config.sops.secrets."tandoor/web_env.enc".path}";
+    DynamicUser = lib.mkForce false;
+    Group = lib.mkForce "tandoor_recipes";
   };
 
   networking.firewall.allowedTCPPorts = [8082];
