27ec2d756709a8e0a178947d61ba26809a8c3ffe
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-01-18 18:00:50 +0100

    Update flake - install umu-launcher

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -1,25 +1,40 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  inherit (pkgs.stdenv.hostPlatform) system;
+  umu = inputs.umu.packages.${system}.umu.override {
+    version = inputs.umu.shortRev;
+    truststore = true;
+    cbor2 = true;
+  };
+in {
   #TODO Create module to automatically activate 32-bit drivers and gamemode when a game-related configuration is used
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    extraCompatPackages = with pkgs; [proton-ge-custom];
-  };
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      extraCompatPackages = with pkgs; [proton-ge-custom];
+    };
 
-  programs.gamemode = {
-    enable = true;
-    settings = {
-      general = {
-        softrealtime = "auto";
-        renice = 4;
-        ioprio = 0;
-        inhibit_screensaver = 1;
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 4;
+          ioprio = 0;
+          inhibit_screensaver = 1;
+        };
       };
     };
   };
+
+  environment.systemPackages = [umu];
 
   # Enable 32 bit OpenGL
   hardware.graphics = {
