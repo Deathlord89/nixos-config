a6fcb58e95391574d160f2d9700cc625ae652e27
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-01-23 17:49:39 +0100

    Switch proton-ge from nyx to nixos repo

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -16,7 +16,10 @@ in {
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-      extraCompatPackages = with pkgs; [proton-ge-custom];
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin # Nixos repo
+        #proton-ge-custom # Chaotic Nyx repo
+      ];
     };
 
     gamemode = {
