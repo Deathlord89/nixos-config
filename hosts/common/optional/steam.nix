9e237507018eb861536fe4d6414118fdd6058bc4
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-02-16 16:04:52 +0100

    Add unzip to steam installation

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -14,6 +14,7 @@ in {
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      extraPackages = with pkgs; [unzip];
       extraCompatPackages = with pkgs; [
         proton-ge-bin # Nixos repo
         steamtinkerlaunch # General tweaks for games
