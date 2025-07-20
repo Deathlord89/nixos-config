cc48993b0f89068acef1ba9deb6f1a5d7f708dc1
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-07-20 18:23:06 +0200

    refactor(chaotic): use default setting options for binary cache

--- hosts/common/base/chaotic-nyx.nix
+++ hosts/common/base/chaotic-nyx.nix
@@ -1,10 +1,3 @@
 {inputs, ...}: {
   imports = [inputs.chaotic.nixosModules.default];
-
-  chaotic.nyx = {
-    # Chaotic-Nyx's binary cache is defined in flake.nix
-    cache.enable = false;
-    # Add Chaotic-Nyx's overlay to system's pkgs
-    overlay.enable = true;
-  };
 }
--- hosts/common/base/nix.nix
+++ hosts/common/base/nix.nix
@@ -19,14 +19,10 @@ in {
       substituters = [
         "https://nix-community.cachix.org"
         #"https://hyprland.cachix.org"
-        "https://chaotic-nyx.cachix.org/"
-        "https://nyx.chaotic.cx/"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
 
       auto-optimise-store = lib.mkDefault true;
