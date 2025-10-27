0d5ba270a54e57d2180df4b3f1573f2eec007b47
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-10-27 20:49:46 +0100

    refactor(database): cleanup and add upgrade hints

--- hosts/NAZGUL/services/database.nix
+++ hosts/NAZGUL/services/database.nix
@@ -1,7 +1,7 @@
 {
   config,
-  pkgs,
   lib,
+  pkgs,
   ...
 }: {
   services = {
@@ -15,6 +15,13 @@
         full_page_writes = "off";
 
         # PGTune
+        # DB Version: 16
+        # OS Type: linux
+        # DB Type: mixed
+        # Total Memory (RAM): 12 GB
+        # CPUs num: 8
+        # Connections num: 200
+        # Data Storage: ssd
         max_connections = "200";
         shared_buffers = "3GB";
         effective_cache_size = "9GB";
@@ -24,7 +31,7 @@
         default_statistics_target = "100";
         random_page_cost = "1.1";
         effective_io_concurrency = "200";
-        work_mem = "1966kB";
+        work_mem = "7561kB";
         huge_pages = "off";
         min_wal_size = "1GB";
         max_wal_size = "4GB";
@@ -39,4 +46,54 @@
       vmOverCommit = true;
     };
   };
+
+  /*
+  PostgreSQL upgrade script.
+
+  - set `newPostgres` below (only upgrade one major version increment at a time!)
+  - deploy it to make the script available
+  - stop all dependent services: `sudo systemctl stop phpfpm-nextcloud listmonk mastodon-sidekiq-all mastodon-web mastodon-streaming-1 mastodon-streaming-2 matrix-synapse atticd hedgedoc markov-bot-telegram`
+  - run `sudo upgrade-pg-cluster`
+  - set new package in PostgreSQL module above
+  - re-deploy
+    - note that the binary cache will still be down at this point, causing errors
+    - either use `--option substitute false`
+    - or temporarily turn it off in `home/base.nix`
+  - recreate optimiziations using `vacuumdb --all --analyze-in-stages` as the script suggests
+  - check health, then delete old folder
+
+  See:
+  https://nixos.org/manual/nixos/stable/#module-services-postgres-upgrading
+  https://git.eisfunke.com/config/nixos/-/blob/46be2c5cd340d313917a0bd30fc29e6750d1747f/nixos/server/db.nix#L120
+  */
+  environment.systemPackages = let
+    # PostgreSQL version to upgrade to, including extensions if any
+    newPostgres = null;
+    cfg = config.services.postgresql;
+  in
+    lib.lists.optional (newPostgres != null) (
+      pkgs.writeShellScriptBin "upgrade-pg-cluster" ''
+        set -eux
+
+        systemctl stop postgresql
+
+        export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+        export NEWBIN="${newPostgres}/bin"
+        export OLDDATA="${cfg.dataDir}"
+        export OLDBIN="${config.services.postgresql.finalPackage}/bin"
+
+        # create and enter new data dir
+        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+        cd "$NEWDATA"
+
+        # initialize new db
+        sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+        # import old db data
+        sudo -u postgres $NEWBIN/pg_upgrade \
+          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+          --old-bindir $OLDBIN --new-bindir $NEWBIN \
+          "$@"
+      ''
+    );
 }
