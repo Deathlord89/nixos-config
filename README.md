8944672890c22385f018b1d6ab3c88f271512bee
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-10-30 20:36:48 +0100

    feat(emacs): disable emacs for now

---
+++ README.md
@@ -0,0 +1,31 @@
+## Notes on reinstalling NixOS
+Find out the hard disk ID with `find /dev/disk/by-id/`.
+This is needed if the [disko](https://github.com/nix-community/disko) config file must be edited.
+
+## Installation of an already configured machine
+- Set the hostname of the machine as ENV variable `export HOST=xyz`.
+
+- Download the disk declaration from github as /tmp/disko.nix directory: `curl https://raw.githubusercontent.com/Deathlord89/nixos-config/main/hosts/$HOST/disko.nix -o /tmp/disko.nix`
+
+```bash
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode destroy,format,mount /tmp/disko.nix
+
+nix-shell -p git
+
+`nixos-install --root /mnt --flake "https://github.com/Deathlord89/nixos-config/archive/master.tar.gz#$HOST"
+```
+
+```bash
+cd /
+umount -Rl /mnt
+zpool export -a
+```
+
+```bash
+nix-shell -p ssh-to-age --run 'cat /etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age'
+```
+
+## Troubleshooting
+- If something fail `nix-collect-garbage`, check the config and reinstall
+- For compiling packages like the nvidia proprietary driver you can override the installer temp directory with: `export TMPDIR=/tmp`
+- Sometimes gnupg refuses my yubikey gpg key. This will fix it: `echo "test" | gpg --clearsign`
--- home/ma-gerbig/optional/emacs/default.nix
+++ home/ma-gerbig/optional/emacs/default.nix
@@ -3,73 +3,81 @@
   pkgs,
   lib,
   ...
-}: {
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs; # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+}: let
+  cfg = config.productivity.emacs;
+in {
+  options.productivity.emacs = {
+    enable = lib.mkEnableOption "enable emacs";
   };
 
-  services.emacs = {
-    enable = true;
-    startWithUserSession = "graphical";
-  };
-
-  systemd.user.services.emacs = {
-    serviceConfig = {
-      TimeoutStartSec = "7min";
+  config = lib.mkIf cfg.enable {
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs; # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
     };
-    #restartTriggers = [
-    # FIXME: this method is not working ...
-    #"${config.home.homeDirectory}/.emacs.d/early-init.el"
-    #"${config.home.homeDirectory}/.emacs.d/init.el"
-    #"${config.home.file.".emacs.d/early-init.el".destination}"
-    #"${config.home.file.".emacs.d/init.el".destination}"
-    #];
-  };
 
-  home.file = {
-    "${config.xdg.configHome}/emacs/early-init.el".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/early-init.el";
-
-    "${config.xdg.configHome}/emacs/init.el".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/init.el";
-
-    "${config.xdg.cacheHome}/emacs/straight/versions/default.el".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/straight-el/default.el";
-
-    "${config.xdg.configHome}/emacs/themes/doom-stylix-theme.el".source = config.lib.stylix.colors {
-      template = builtins.readFile ./themes/doom-stylix-theme.el.mustache;
-      extension = ".el";
+    services.emacs = {
+      enable = true;
+      startWithUserSession = "graphical";
     };
-  };
 
-  home.packages = with pkgs; [
-    # Straight.el
-    python3
-    watchexec
+    systemd.user.services.emacs = {
+      serviceConfig = {
+        TimeoutStartSec = "7min";
+      };
+      #restartTriggers = [
+      # FIXME: this method is not working ...
+      #"${config.home.homeDirectory}/.emacs.d/early-init.el"
+      #"${config.home.homeDirectory}/.emacs.d/init.el"
+      #"${config.home.file.".emacs.d/early-init.el".destination}"
+      #"${config.home.file.".emacs.d/init.el".destination}"
+      #];
+    };
 
-    # IDE
-    nil
+    home.file = {
+      "${config.xdg.configHome}/emacs/early-init.el".source =
+        config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/early-init.el";
 
-    # Doom-modeline symbols
-    nerd-fonts.symbols-only
-  ];
+      "${config.xdg.configHome}/emacs/init.el".source =
+        config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/init.el";
 
-  # Clean up unwanted OS defaults
-  home.activation = {
-    deleteDotEmacs = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      DOTEMACS="${config.home.homeDirectory}/.emacs"
-      DOTEMACSDIR="${config.home.homeDirectory}/.emacs.d"
-      if [[ -e "$DOTEMACS" ]]; then
-      rm -f "$DOTEMACS"
-      fi
+      "${config.xdg.cacheHome}/emacs/straight/versions/default.el".source =
+        config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/git/nixos-config/home/ma-gerbig/optional/emacs/straight-el/default.el";
 
-      if [[ -d "$DOTEMACSDIR" ]]; then
-      rm -rf "$DOTEMACSDIR"
-      fi
-    '';
+      "${config.xdg.configHome}/emacs/themes/doom-stylix-theme.el".source = config.lib.stylix.colors {
+        template = builtins.readFile ./themes/doom-stylix-theme.el.mustache;
+        extension = ".el";
+      };
+    };
+
+    home.packages = with pkgs; [
+      # Straight.el
+      python3
+      watchexec
+
+      # IDE
+      nil
+
+      # Doom-modeline symbols
+      nerd-fonts.symbols-only
+    ];
+
+    # Clean up unwanted OS defaults
+    home.activation = {
+      deleteDotEmacs = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        DOTEMACS="${config.home.homeDirectory}/.emacs"
+        DOTEMACSDIR="${config.home.homeDirectory}/.emacs.d"
+        if [[ -e "$DOTEMACS" ]]; then
+        rm -f "$DOTEMACS"
+        fi
+
+        if [[ -d "$DOTEMACSDIR" ]]; then
+        rm -rf "$DOTEMACSDIR"
+        fi
+      '';
+    };
   };
 }
