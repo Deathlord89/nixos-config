56444ea85001b481160d7389c68be2ab376a90d7
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-10-30 16:33:50 +0100

    feat(home-manager): reverting set firefox as default browser

--- home/ma-gerbig/optional/firefox.nix
+++ home/ma-gerbig/optional/firefox.nix
@@ -2,28 +2,29 @@
   config,
   pkgs,
   ...
-}: let
-  browser = "firefox.desktop";
-in {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/x-extension-htm" = [browser];
-      "application/x-extension-html" = [browser];
-      "application/x-extension-shtml" = [browser];
-      "application/x-extension-xht" = [browser];
-      "application/x-extension-xhtml" = [browser];
-      "application/xhtml+xml" = [browser];
-      "application/json" = [browser];
-      "text/html" = [browser];
-      "x-scheme-handler/about" = [browser];
-      "x-scheme-handler/ftp" = [browser];
-      "x-scheme-handler/http" = [browser];
-      "x-scheme-handler/https" = [browser];
-      "x-scheme-handler/unknown" = [browser];
-    };
-  };
-
+}:
+#let
+#browser = "firefox.desktop";
+#in {
+#xdg.mimeApps = {
+#  enable = true;
+#  defaultApplications = {
+#    "application/x-extension-htm" = [browser];
+#    "application/x-extension-html" = [browser];
+#    "application/x-extension-shtml" = [browser];
+#    "application/x-extension-xht" = [browser];
+#    "application/x-extension-xhtml" = [browser];
+#    "application/xhtml+xml" = [browser];
+#    "application/json" = [browser];
+#    "text/html" = [browser];
+#    "x-scheme-handler/about" = [browser];
+#    "x-scheme-handler/ftp" = [browser];
+#    "x-scheme-handler/http" = [browser];
+#    "x-scheme-handler/https" = [browser];
+#    "x-scheme-handler/unknown" = [browser];
+#  };
+#};
+{
   programs.firefox = {
     enable = true;
 
